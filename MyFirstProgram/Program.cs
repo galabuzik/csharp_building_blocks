06c56b1a2c552505f59023af710c8890a3614ba3
Kelvin Galabuzi Lukoda <kelvingalabuzi@gmail.com>
2021-08-08 12:11:02 +0300

    merge projects

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyUtilities;
+
+namespace MyFirstProgram
+{
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where should we go in May?");
+            WeatherUtiltites.Report("San Francisco", WeatherUtiltites.FahrenheitToCelsius(65), 73);
+            WeatherUtiltites.Report("Denver", WeatherUtiltites.FahrenheitToCelsius(77), 55);
+            WeatherUtiltites.Report("Bologna", 23, 65);
+        }
+  
+    }
+}
---
+++ MyFirstProgram/WeatherUtilities.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace MyUtilities
+{
+    class WeatherUtiltites
+    {
+        static public float FahrenheitToCelsius(float temperatureFahrenheit)
+        {
+            return (temperatureFahrenheit - 32) / 1.8f;
+        }
+
+        static public float CelsiusToFahrenheit(float temperatureCelsius)
+        {
+            return temperatureCelsius * 1.8f + 32;
+        }
+        static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        {
+            // Not very reliable formula
+            return (temperatureFahrenheit + humidityPercent) / 4;
+
+        }
+        static public void Report(string location, float temperatureCelsius, float humidity)
+        {
+            var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
+            Console.WriteLine($"Comfort Index for{location}:{ComfortIndex(temperatureFahrenheit, humidity)}");
+
+        }
+    }
+        
+
+
+
+}
