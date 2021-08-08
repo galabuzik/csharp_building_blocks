5a082388f155a47500f230c48ea648293d07a4e3
Kelvin Galabuzi Lukoda <kelvingalabuzi@gmail.com>
2021-08-08 13:54:50 +0300

    tested readline

---
+++ codeacademy-track-01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace codeacademy_track_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+        }
+    }
+}
