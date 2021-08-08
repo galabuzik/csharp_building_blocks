204c952fbb6a99f879b0815f92a6574b30b8e16a
Kelvin Galabuzi Lukoda <kelvingalabuzi@gmail.com>
2021-08-08 12:07:25 +0300

    merge projects
    Merged previous csharp projects into central repository

---
+++ wiredbraincoffeesurvey/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateTasksReport();
+        }
+
+        private static void GenerateTasksReport()
+        {
+            var tasks = new List<string>();
+
+            // Calculated Values
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+            Console.WriteLine($"Response Percentage:{responseRate}");
+            Console.WriteLine($"Overall Score:{overallScore}");
+
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients.");
+            }
+            if (overallScore > 8.0)
+            {
+                tasks.Add("Work with leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("Work with employess for improvement ideas.");
+            }
+
+            if (responseRate < .33)
+            {
+                tasks.Add("Research options to improve response rate.");
+            }
+            else if (responseRate > .33 && responseRate < .66)
+            {
+                tasks.Add("Reward Participants with free coffee coupon");
+            }
+            else
+            {
+                tasks.Add("Rewars Participants with discount coffee coupon");
+            }
+
+            switch (Q1Results.AreaToImprove)
+            {
+                case "RewardsProgram":
+                    tasks.Add("Revisit the rewards deals.");
+                    break;
+
+                case "Cleanliness":
+                    tasks.Add("Contact the cleaning vendor.");
+                    break;
+
+                case "MobileApp":
+                    tasks.Add("Contact consulting firm about app.");
+                    break;
+
+                default:
+                    tasks.Add("Investigate individual comments for ideas");
+                    break;
+
+
+            }
+        }
+
+
+    }
+}
